708991f39415db55231a7075a8b5f7235bbe9591
vhu1 <bhuwan7b@gmail.com>
2020-05-18 15:50:16 +0545

    configuring class and interface for factory design pattern

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,16 +17,34 @@ namespace Graphics_Object
         {
             InitializeComponent();
         }
-
-        public static int add(int a , int b)
+        private void Form1_Load(object sender, EventArgs e )
         {
-            return a + b;
+
+            string[] passedInArgs = Environment.GetCommandLineArgs();
+            if(passedInArgs.Length < 2)
+            {
+                MessageBox.Show("Please pass and valid argument to load shapes");
+                this.Close();
+                return;
+            }
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            int b = add(2, 5);
-            label1.Text = ""+b;
+            string[] passedInArgs = Environment.GetCommandLineArgs();
+            if (passedInArgs.Contains("run"))
+            {
+
+                GetShapeFactory g = new GetShapeFactory();
+                g.getShape(passedInArgs[2]).draw(e);
+
+            }
+            else
+            {
+                MessageBox.Show("Please pass and valid argument to load shapes");
+                this.Close();
+            }
         }
     }
 }
---
+++ Shape.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphics_Object
+{
+    interface CustomObject
+    {
+        void draw(PaintEventArgs e);
+    }
+    class Circle : CustomObject
+    {
+        public void draw(PaintEventArgs e)
+        {
+            Pen red = new Pen(Color.Red);
+            Rectangle circle = new Rectangle(20, 20, 220, 220);
+            Graphics g = e.Graphics;
+            g.DrawEllipse(red, circle);
+        }
+    }
+    class Triangle : CustomObject
+    {
+        public void draw(PaintEventArgs e)
+        {
+            Pen red = new Pen(Color.Red);
+            Graphics g = e.Graphics;
+            g.DrawLine(red, 10, 10, 20, 10);
+            g.DrawLine(red, 10, 10, 10, 50);
+            g.DrawLine(red, 10, 50, 20, 10);
+        }
+    }
+
+    class GetShapeFactory
+    {
+        public CustomObject getShape(String geomObj)
+        {
+            if(geomObj == null)
+            {
+                return null;
+            }
+            if(string.Equals(geomObj , "circle", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Circle();
+            }
+            if(string.Equals(geomObj , "triangle", StringComparison.OrdinalIgnoreCase))
+            {
+                return new Triangle();
+            }
+            return null;
+        }
+    }
+}
